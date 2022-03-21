e48813f1d8afb43f037347097f4be61640eba877
miguins <lucasmiguins@gmail.com>
2022-03-21 16:11:33 -0300

    Configuring jest and adding inital test

--- src/schema.ts
+++ src/schema.ts
@@ -2,7 +2,7 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { productTypeDefs } from './modules/products/graphql/products.typedefs';
 import resolvers from './resolvers';
 
-const typeDefs = [
+export const typeDefs = [
   productTypeDefs
 ]
 
