dbf76c0635f9c9150e5611e11b127bafc7682a6c
miguins <lucasmiguins@gmail.com>
2022-03-29 16:06:34 -0300

    Fixing types

--- src/modules/users/integration/MockApiUsersClient.ts
+++ src/modules/users/integration/MockApiUsersClient.ts
@@ -1,12 +1,13 @@
+import { User } from '@models/User';
 import axios from 'axios';
 import { injectable } from 'tsyringe';
 
 @injectable()
 export class MockApiUsersClient {
 
-  async getUsers() {
+  async getUsers(): Promise<User[]> {
     try {
-      const response = await axios.get('https://6243482a39aae3e3b7413750.mockapi.io/api/users');
+      const response = await axios.get<User[]>('https://6243482a39aae3e3b7413750.mockapi.io/api/users');
       // console.log(response);
       return response.data
     } catch (error) {
--- src/modules/users/services/GetUsersService.ts
+++ src/modules/users/services/GetUsersService.ts
@@ -1,3 +1,4 @@
+import { User } from '@models/User';
 import 'reflect-metadata';
 import { container, injectable } from 'tsyringe';
 import { MockApiUsersClient } from '../integration/MockApiUsersClient';
@@ -5,7 +6,7 @@ import { MockApiUsersClient } from '../integration/MockApiUsersClient';
 @injectable()
 export class GetUsersService {
 
-  async execute() {
+  async execute(): Promise<User[]> {
     const usersClientResponse = container.resolve(MockApiUsersClient)
     const users = usersClientResponse.getUsers()
 
